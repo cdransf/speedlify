e0ebfa51936abb2452166332ad6e929f84e86ec3
Cory Dransfeldt <cory.dransfeldt@gmail.com>
2023-12-17 16:08:48 -0800

    chore: site fix

--- _data/sites/coryd.js
+++ _data/sites/coryd.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "coryd.dev",
 	description: "Cory's personal web site",
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: !process.env.CONTEXT,
 	options: {
 		frequency: 60 * 23,
 		freshChrome: "site"
