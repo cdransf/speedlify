3d1b0088eef814adcd4107251ffdd27db8147324
Cory Dransfeldt <coryd@hey.com>
2024-03-05 09:23:27 -0800

    chore: add posts

--- _data/sites/posts.js
+++ _data/sites/posts.js
@@ -12,5 +12,7 @@ module.exports = {
 		"https://coryd.dev/posts/2024/i-found-the-music-i-love-on-the-internet/",
 		"https://coryd.dev/posts/2024/i-dont-want-anything-your-ai-generates/",
     "https://coryd.dev/posts/2024/renting-your-music-means-accepting-that-it-will-disappear/",
+    "https://coryd.dev/posts/2024/im-going-to-keep-opting-out/",
+    "https://coryd.dev/posts/2024/towards-a-quieter-friendlier-web/",
 	]
 };
