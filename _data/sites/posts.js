3aece7d1f446d91fa0d934a1d751b2cbe300b23c
Cory Dransfeldt <coryd@hey.com>
2024-03-10 21:59:57 -0700

    chore: update posts

--- _data/sites/posts.js
+++ _data/sites/posts.js
@@ -11,8 +11,9 @@ module.exports = {
 		"https://coryd.dev/posts/2023/i-dont-want-streaming-music/",
 		"https://coryd.dev/posts/2024/i-found-the-music-i-love-on-the-internet/",
 		"https://coryd.dev/posts/2024/i-dont-want-anything-your-ai-generates/",
-    "https://coryd.dev/posts/2024/renting-your-music-means-accepting-that-it-will-disappear/",
-    "https://coryd.dev/posts/2024/im-going-to-keep-opting-out/",
-    "https://coryd.dev/posts/2024/towards-a-quieter-friendlier-web/",
+                "https://coryd.dev/posts/2024/renting-your-music-means-accepting-that-it-will-disappear/",
+                "https://coryd.dev/posts/2024/im-going-to-keep-opting-out/",
+                "https://coryd.dev/posts/2024/towards-a-quieter-friendlier-web/",
+		"https://coryd.dev/posts/2024/of-course-ai-is-extractive-everything-is-lately/",
 	]
 };
