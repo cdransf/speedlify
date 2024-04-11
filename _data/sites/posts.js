5b047671bdda816b99391a8f61c5128f180b570f
Cory Dransfeldt <coryd@hey.com>
2024-04-11 07:38:35 -0700

    chore: update posts

--- _data/sites/posts.js
+++ _data/sites/posts.js
@@ -16,5 +16,9 @@ module.exports = {
     "https://coryd.dev/posts/2024/towards-a-quieter-friendlier-web/",
     "https://coryd.dev/posts/2024/the-internet-isnt-dying-its-changing/",
     "https://coryd.dev/posts/2024/we-have-a-content-quality-problem-not-a-content-quantity-problem/",
+    "https://coryd.dev/posts/2024/dont-be-afraid-to-admin-when-you-dont-know-something/",
+    "https://coryd.dev/posts/2024/a-retrospective-on-a-year-without-streaming-music/",
+    "https://coryd.dev/posts/2024/data-collection-should-always-be-opt-in/",
+    "https://coryd.dev/posts/2024/ai-cannot-and-should-not-replace-search/",
   ]
 };
